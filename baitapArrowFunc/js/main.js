19933d12e2588f59c609bf1765d45f9a2aa0d156
lamhungtandung <lamhungtandung@gmail.com>
2022-08-23 20:38:56 +0700

    s

--- baitapArrowFunc/js/main.js
+++ baitapArrowFunc/js/main.js
@@ -1,2 +1,40 @@
-import selectColorList from "./baitap1.js";
-selectColorList();
+const colorList = [
+    "pallet",
+    "viridian",
+    "pewter",
+    "cerulean",
+    "vermillion",
+    "lavender",
+    "celadon",
+    "saffron",
+    "fuschia",
+    "cinnabar"
+];
+let getELE = id => document.getElementById(id);
+
+homeColor = (colorList) => {
+    let content = "";
+
+    colorList.map((colorS, index) => {
+
+        content += `
+            <button class= "color-button ${colorS}" onclick= "changeColor('${colorS}')"></button>
+        `;
+    });
+
+    getELE("colorContainer").innerHTML = content;
+
+}
+
+homeColor(colorList);
+
+changeColor = (myColor) => {
+    let checkColor = getELE("house").classList;
+    
+    if (checkColor == "house") {
+        getELE("house").classList.add(myColor);
+    } else {
+        getELE("house").classList = "";
+        getELE("house").classList.add("house", myColor);
+    }
+}
